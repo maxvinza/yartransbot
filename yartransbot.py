cfbca0b21d9213c306ceae460f4e4cd3559b20fc
Oleg Elifantiev <oleg@elifantiev.ru>
2018-11-14 23:05:21 +0300

    Beta

--- yartransbot.py
+++ yartransbot.py
@@ -1,24 +1,48 @@
 # -*- coding: utf-8 -*-
-import telebot
-import string
 import urllib.request
 import re
-import requests
+import os
+import logging
 from bs4 import BeautifulSoup
-telebot.apihelper.proxy = {'https': 'socks5://telegram:telegram@qzkjd.teletype.live:443'}
-bot = telebot.TeleBot('KEY')
+from telegram import Bot
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Введите номер интересующего маршрута вида а23 или автобус23, тр1, тм1, мт96  ")
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    input_mess = razbor_msg(message.text)
+TOKEN = os.environ.get('TOKEN', 'TOKEN')
+PORT = int(os.environ.get('PORT', '8443'))
+APP_NAME = 'yar-transport-tg-bot'
+
+
+def send_welcome(bot: Bot, update):
+    message = update.message.text
+    chat_id = update.message.chat_id
+    bot.send_message(chat_id=chat_id,
+                     text="Введите номер интересующего маршрута вида а23 или автобус23, тр1, тм1, мт96  ")
+
+
+def echo_all(bot: Bot, update):
+    input_mess = razbor_msg(update.message.text)
     out_mess = pars(input_mess)
-    bot.reply_to(message, out_mess)
-    
-    
+    bot.send_message(chat_id=update.message.chat_id, text=out_mess)
+
+
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', send_welcome))
+dispatcher.add_handler(MessageHandler(Filters.text, echo_all))
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.set_webhook("https://{0}.herokuapp.com/{1}".format(APP_NAME, TOKEN))
+updater.idle()
+
+
 def razbor_msg(mess):
     mess = mess.lower()
     mess = (mess.replace(" ", ""))
@@ -29,9 +53,10 @@ def razbor_msg(mess):
     mess = (mess.replace("маршрутка", "мт"))
     mess = (mess.replace("трамвай", "тм"))
     return mess
-    
+
+
 def pars(mess):
-    print (mess)
+    print(mess)
     need_try_mt = 0
     if mess[0] == '/':
         return command_bot(mess)
@@ -51,7 +76,7 @@ def pars(mess):
             return "Уточните вид трансорта, например а" + num + " - это будет соответствовать автобусу " + num
     else:
         need_try_mt = 1
-    url = "http://ot76.ru/mob/getroutestr.php?vt="+str(vt)+"&nmar="+str(num)
+    url = "http://ot76.ru/mob/getroutestr.php?vt=" + str(vt) + "&nmar=" + str(num)
     if need_try_mt == 1:
         url = kostili_marsh(mess)
         if url == "err":
@@ -60,8 +85,9 @@ def pars(mess):
     if len(htext) < 2:
         htext = 'Опять от тебя сбежала последняя электричка. Скорее всего, на маршруте сейчас никого.'
     return htext
-    
-#Определение вида транспорта по номеру
+
+
+# Определение вида транспорта по номеру
 def only_num(num):
     num = int(num)
     if num < 10:
@@ -89,7 +115,8 @@ def only_num(num):
         else:
             return 0
 
-#Разбор нетепичных маршрутов - вроде тех, которы с литерами    
+
+# Разбор нетепичных маршрутов - вроде тех, которы с литерами
 def kostili_marsh(mess):
     if mess == 'а18к':
         url = "http://ot76.ru/mob/getroutestr.php?vt=1&nmar=18k"
@@ -134,36 +161,38 @@ def kostili_marsh(mess):
     else:
         url = "err"
     return url
-    
 
 
 def stand_replace(url):
     htext = pars_replace(url)
     test_text = skobka_perenos(htext)
-    soup =  BeautifulSoup(test_text, 'html.parser')
+    soup = BeautifulSoup(test_text, 'html.parser')
     htext = soup.get_text()
-    htext = (htext.replace("\n",""))
-    htext = (htext.replace("Прямо","\nПрямо"))
-    htext = (htext.replace("Обрат","\nОбрат"))
-    htext = (htext.replace("Борт","\nБорт"))
-    htext = (htext.replace("Расписание","\n")) 
-    htext = (htext.replace("МУП ГПТ \"Яргортранс\"",""))
-    htext = (htext.replace("№БортТекущая остановкаВр.ост.Задержка, мин.","")) #getpeinfo.php?vt=
-    htext = (htext.replace("getpeinfo.php?vt=","/m"))
-    htext = (htext.replace("None",""))
-    htext = (htext.replace("Прогноз прохождения","\n\n"))
-    htext = (htext.replace("&npe;=","num")) #Прогноз прохождения
-    htext = (htext.replace("К списку маршрутовнп - Низкопольное ТСпнп - Полунизкопольное ТСм/моб - Высокопольное ТС, оборудованное для перевозки маломобильных групп населениянул. - ТС следует в/из депо",""))
-    htext = (htext.replace("===","\n"))
+    htext = (htext.replace("\n", ""))
+    htext = (htext.replace("Прямо", "\nПрямо"))
+    htext = (htext.replace("Обрат", "\nОбрат"))
+    htext = (htext.replace("Борт", "\nБорт"))
+    htext = (htext.replace("Расписание", "\n"))
+    htext = (htext.replace("МУП ГПТ \"Яргортранс\"", ""))
+    htext = (htext.replace("№БортТекущая остановкаВр.ост.Задержка, мин.", ""))  # getpeinfo.php?vt=
+    htext = (htext.replace("getpeinfo.php?vt=", "/m"))
+    htext = (htext.replace("None", ""))
+    htext = (htext.replace("Прогноз прохождения", "\n\n"))
+    htext = (htext.replace("&npe;=", "num"))  # Прогноз прохождения
+    htext = (htext.replace(
+        "К списку маршрутовнп - Низкопольное ТСпнп - Полунизкопольное ТСм/моб - Высокопольное ТС, оборудованное для перевозки маломобильных групп населениянул. - ТС следует в/из депо",
+        ""))
+    htext = (htext.replace("===", "\n"))
     return htext
+
 
 # Другая ветка - разбор сообщения о для вызова комманды бота
 def command_bot(mess):
     if len(mess) < 5:
         return "Пустая команда"
     if mess[1] == 'm' and mess[3] == 'n':
-        mess = (mess.replace("/m","getpeinfo.php?vt="))
-        mess = (mess.replace("num","&npe="))
+        mess = (mess.replace("/m", "getpeinfo.php?vt="))
+        mess = (mess.replace("num", "&npe="))
         url = "http://ot76.ru/mob/" + mess
         mess = stand_replace(url)
         if len(mess) < 2:
@@ -171,19 +200,21 @@ def command_bot(mess):
         return mess
     else:
         return "Неправильная комманда"
- 
+
+
 def pars_replace(url):
-    #try:
-    pmd=urllib.request.urlopen(url)
-    #except requests.exceptions:
+    # try:
+    pmd = urllib.request.urlopen(url)
+    # except requests.exceptions:
     #    return "Неправильная комманда"
-    htext=pmd.read()
-    htext=htext.decode('cp1251')
+    htext = pmd.read()
+    htext = htext.decode('cp1251')
     return htext
 
-#Разбор html-структуры страницы
+
+# Разбор html-структуры страницы
 def skobka_perenos(text):
-    soup =  BeautifulSoup(text, 'html.parser')
+    soup = BeautifulSoup(text, 'html.parser')
     textl = soup.find_all('tr')
     retstr = ''
     for strk in textl:
@@ -191,26 +222,26 @@ def skobka_perenos(text):
         retstr = retstr + razbor_td(str(strk))
     return retstr
 
+
 def razbor_td(tr):
-    soup =  BeautifulSoup(tr, 'html.parser')
+    soup = BeautifulSoup(tr, 'html.parser')
     textl = soup.find_all('td')
     if len(textl) > 2:
-        return get_text(str(textl[1])) + " - " + get_text(str(textl[2])) + " - " + get_text(str(textl[3])) + " - " + str(make_ts(str(textl[5])))  + "===" #str(make_ts(str(textl[3]))) + "\n"
+        return get_text(str(textl[1])) + " - " + get_text(str(textl[2])) + " - " + get_text(
+            str(textl[3])) + " - " + str(make_ts(str(textl[5]))) + "==="  # str(make_ts(str(textl[3]))) + "\n"
     elif len(textl) == 2:
         return get_text(str(textl[0])) + " - " + get_text(str(textl[1])) + "==="
     else:
         return tr
-        
+
+
 def get_text(tsh):
-    soup =  BeautifulSoup(tsh, 'html.parser')
+    soup = BeautifulSoup(tsh, 'html.parser')
     return soup.get_text()
-        
+
+
 # На будущее - разбор url
 def make_ts(tsh):
-    soup =  BeautifulSoup(tsh, 'html.parser')
+    soup = BeautifulSoup(tsh, 'html.parser')
     for link in soup.find_all('a'):
-        return(link.get('href'))
-
-
-    
-bot.polling()
+        return (link.get('href'))
